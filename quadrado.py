07f7ce29b2416ee497320a7fe4688f5f4c5f9d94
Lucas Dircksen Balcevicz <145569604+Lucas-dirbal@users.noreply.github.com>
2025-02-24 15:25:50 -0300

    Create quadrado.py

---
+++ quadrado.py
@@ -0,0 +1,8 @@
+print("*****************")
+print("Desafio de Python")
+print("*****************")
+    
+lado_1 = int(input("Qual o lado do Quadrado ? 'em centimetros' "))
+area = lado_1 * 2
+perimetro = lado_1 * 4
+print(f"A área de um quadrado de {lado_1} centimetros é {area}\nCom o perimetro de {perimetro}")
