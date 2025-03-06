8e585336c7e8ae6108043f918d1d4293bcf1c5d3
Lucas Dircksen Balcevicz <145569604+Lucas-dirbal@users.noreply.github.com>
2025-03-06 17:45:19 +0000

    a

--- forca.py
+++ forca.py
@@ -1,21 +1,23 @@
-palavraSecreta = "amora"
+palavraSecreta = "amora".upper()
 
 letrasAcertadas = ["_","_","_","_","_"]
 print(letrasAcertadas)
 
 enforcou = False
 acertou = False
+erros = 0
 
 while(not enforcou and not acertou):
     
     chute = input("Qual a letra? ")
-    chute = chute.strip()
+    chute = chute.strip().upper()
 
-    index = 0
+    if (chute in palavraSecreta):
+        index = 0
 
-    for letra in palavraSecreta:
-        if(chute.upper() == letra.upper()):
-            letrasAcertadas[index] = letra
-        index = index + 1
+        for letra in palavraSecreta:
+            if(chute.upper() == letra.upper()):
+                letrasAcertadas[index] = letra
+            index = index + 1
     print(letrasAcertadas)
 print("Fim de Jogo")
