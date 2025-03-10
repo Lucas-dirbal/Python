febe790690d7db17e0dfbd8955877370e711715e
Lucas Dircksen Balcevicz <145569604+Lucas-dirbal@users.noreply.github.com>
2025-03-10 18:01:01 +0000

    a

---
+++ baskara.py
@@ -0,0 +1,8 @@
+import math
+a = float(input("Qual o valor de A ?\n"))
+b = float(input("Qual o valor de B ?\n"))
+c = float(input("Qual o valor de C ?\n"))
+delta = (b ** 2 ) - (4 * a * c)
+x1 = (-b + math.sqrt(delta)) / (a*2)
+x2 = (-b - math.sqrt(delta)) / (a*2)
+print(f"O x1 é {x1} e o x2 é {x2}")
