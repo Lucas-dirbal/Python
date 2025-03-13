c99e6ad2633b424da3ff41b93306b6aad6b87bd7
Lucas Dircksen Balcevicz <145569604+Lucas-dirbal@users.noreply.github.com>
2025-03-13 17:09:42 +0000

    a

--- baskara.py
+++ baskara.py
@@ -3,6 +3,9 @@ a = float(input("Qual o valor de A ?\n"))
 b = float(input("Qual o valor de B ?\n"))
 c = float(input("Qual o valor de C ?\n"))
 delta = (b ** 2 ) - (4 * a * c)
-x1 = (-b + math.sqrt(delta)) / (a*2)
-x2 = (-b - math.sqrt(delta)) / (a*2)
-print(f"O x1 é {x1} e o x2 é {x2}")
+if (delta < 0) ;
+    print("Delta menor que 0")
+else:
+    x1 = (-b + math.sqrt(delta)) / (a*2)
+    x2 = (-b - math.sqrt(delta)) / (a*2)
+    print(f"O x1 é {x1} e o x2 é {x2}")
