ba11f7073c0a01fc7baf06f57eb30f18ce5b728d
Lucas Dircksen Balcevicz <145569604+Lucas-dirbal@users.noreply.github.com>
2025-03-27 17:41:48 +0000

    aa

--- nuBank.py
+++ nuBank.py
@@ -1,8 +1,12 @@
-
-
-
 def criaConta(numero, titular, saldo, limite):
     conta = {"numero": numero, "titular": titular, "saldo": saldo, "limite": limite}
     return conta
 
-    
+def deposita(conta, valor):
+    conta["saldo"] += valor
+
+def saca(conta, valor):
+    conta["saldo"] -= valor
+
+def extrato(conta):
+    print("numero: {} \nsaldo: {}".format(conta["numero"], conta["saldo"]))
