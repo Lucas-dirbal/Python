6b5e008d56cc595afb2e7e0dc2f66f116de4ae80
Lucas Dircksen Balcevicz <145569604+Lucas-dirbal@users.noreply.github.com>
2025-04-24 17:11:48 +0000

    a

--- streaming.py
+++ streaming.py
@@ -10,12 +10,16 @@ class Filme:
     def likes(self):
         return self.__likes
     
-    def def_likes():
+    def def_likes(self):
         self.__likes += 1
 
     @property
     def nome(self):
         return self.__nome
+
+    @property
+    def imdb(self):
+        return self.__imdb
     
     @imdb.setter
     def imdb(self, imdb):
@@ -24,3 +28,20 @@ class Filme:
     @nome.setter
     def nome(self, nome):
         self.__nome = nome
+
+class Serie:
+    def __init__(self, nome, ano , temporadas, imdb):
+        self.__nome = nome
+        self.ano = ano
+        self.__temporadas = temporadas
+        self.imdb = imdb
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
+    
+    def def_likes(self):
+        self.__likes += 1
+
+    
---
+++ zecaurubudopix.py
@@ -0,0 +1,3 @@
+valorCompra = float(input("Digite o valor de compra: "))
+print(f"Valor da compra: ", valorCompra)
+valorVenda = float(input("Digite o valor de venda: "))
