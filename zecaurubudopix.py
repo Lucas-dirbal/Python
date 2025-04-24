8bc88319ca079a417851483b901b1f5af0007200
Lucas Dircksen Balcevicz <145569604+Lucas-dirbal@users.noreply.github.com>
2025-04-24 17:57:46 +0000

    a

--- zecaurubudopix.py
+++ zecaurubudopix.py
@@ -9,15 +9,19 @@ invsInicial = float(input("Qual o investimento inicial? "))
 taxa = float(input("Qual a taxa de desconto ? "))
 op = input("Quantos faturamento você quer calcular? (1, 2, 3 ou 4) ")
 if op == "1":
+    op = 1
     faturamento1 = float(input("Qual o faturamento do ano 1? "))
 elif op == "2":
+    op = 2
     faturamento1 = float(input("Qual o faturamento do ano 1? "))
     faturamento2 = float(input("Qual o faturamento do ano 2? "))
 elif op == "3":
+    op = 3
     faturamento1 = float(input("Qual o faturamento do ano 1? "))
     faturamento2 = float(input("Qual o faturamento do ano 2? "))
     faturamento3 = float(input("Qual o faturamento do ano 3? "))
 elif op == "4":
+    op = 4
     faturamento1 = float(input("Qual o faturamento do ano 1? "))
     faturamento2 = float(input("Qual o faturamento do ano 2? "))
     faturamento3 = float(input("Qual o faturamento do ano 3? "))
@@ -25,3 +29,42 @@ elif op == "4":
 else:
     print("Opção inválida")
     exit()
+
+# tempo elevado por anos 
+if op == 1:
+    VPL1 = faturamento1 / (1 + taxa) ** 1
+    VPL = VPL1 - invsInicial
+
+
+elif op == 2:
+    VPL1 = faturamento1 / (1 + taxa) ** 1
+    VPL = VPL1 - invsInicial
+    VLP2 = faturamento2 / (1 + taxa) ** 2
+    VPL = VPL1 + VLP2 - invsInicial
+
+elif op == 3:
+    VPL1 = faturamento1 / (1 + taxa) ** 1
+    VPL = VPL1 - invsInicial
+    VLP2 = faturamento2 / (1 + taxa) ** 2
+    VPL = VPL1 + VLP2 - invsInicial
+    VLP3 = faturamento3 / (1 + taxa) ** 3
+    VPL = VPL1 + VLP2 + VLP3 - invsInicial
+elif op == 4:
+    VPL1 = faturamento1 / (1 + taxa) ** 1
+    VPL = VPL1 - invsInicial
+    VLP2 = faturamento2 / (1 + taxa) ** 2
+    VPL = VPL1 + VLP2 - invsInicial
+    VLP3 = faturamento3 / (1 + taxa) ** 3
+    VPL = VPL1 + VLP2 + VLP3 - invsInicial
+    VLP4 = faturamento4 / (1 + taxa) ** 4
+    VPL = VPL1 + VLP2 + VLP3 + VLP4 - invsInicial
+else:
+    print("Opção inválida")
+    exit()
+# Exibir o resultado
+print("O VPL é: ", VPL)
+
+if VPL > 0:
+    print("O seu investimeto gerará lucro")
+else:
+    print("O seu investimento não gerará lucro")
