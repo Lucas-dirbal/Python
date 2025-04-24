ef6003e9817f24e0631cfbd27666eb1b6b0a03cf
Lucas Dircksen Balcevicz <145569604+Lucas-dirbal@users.noreply.github.com>
2025-04-24 17:51:12 +0000

    as

--- zecaurubudopix.py
+++ zecaurubudopix.py
@@ -10,7 +10,18 @@ taxa = float(input("Qual a taxa de desconto ? "))
 op = input("Quantos faturamento você quer calcular? (1, 2, 3 ou 4) ")
 if op == "1":
     faturamento1 = float(input("Qual o faturamento do ano 1? "))
-faturamento1 = float(input("Qual o faturamento do ano 1? "))
-faturamento2 = float(input("Qual o faturamento do ano 2? "))
-faturamento3 = float(input("Qual o faturamento do ano 3? "))
-faturamento4 = float(input("Qual o faturamento do ano 4? "))
+elif op == "2":
+    faturamento1 = float(input("Qual o faturamento do ano 1? "))
+    faturamento2 = float(input("Qual o faturamento do ano 2? "))
+elif op == "3":
+    faturamento1 = float(input("Qual o faturamento do ano 1? "))
+    faturamento2 = float(input("Qual o faturamento do ano 2? "))
+    faturamento3 = float(input("Qual o faturamento do ano 3? "))
+elif op == "4":
+    faturamento1 = float(input("Qual o faturamento do ano 1? "))
+    faturamento2 = float(input("Qual o faturamento do ano 2? "))
+    faturamento3 = float(input("Qual o faturamento do ano 3? "))
+    faturamento4 = float(input("Qual o faturamento do ano 4? "))
+else:
+    print("Opção inválida")
+    exit()
