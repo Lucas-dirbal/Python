2405f2278e7370274b20352f6cb8c45505c3257e
Lucas Dircksen Balcevicz <145569604+Lucas-dirbal@users.noreply.github.com>
2025-04-24 17:48:55 +0000

    aa

--- zecaurubudopix.py
+++ zecaurubudopix.py
@@ -1,3 +1,16 @@
-valorCompra = float(input("Digite o valor de compra: "))
-print(f"Valor da compra: ", valorCompra)
-valorVenda = float(input("Digite o valor de venda: "))
+# 1 - Perguntar o investimento inicial
+# 2 - Perguntar a taxa de desconto
+# 3 - Perguntar o faturamento do ano 1, 2, 3 e 4
+# 4 - Calcular o VPL de cada ano segundo a fórmula
+# 5 - faturamento1 / (1 + taxa) ^ tempo
+# 6 - somar os VPLs e dimunir o investimento inicial
+
+invsInicial = float(input("Qual o investimento inicial? "))
+taxa = float(input("Qual a taxa de desconto ? "))
+op = input("Quantos faturamento você quer calcular? (1, 2, 3 ou 4) ")
+if op == "1":
+    faturamento1 = float(input("Qual o faturamento do ano 1? "))
+faturamento1 = float(input("Qual o faturamento do ano 1? "))
+faturamento2 = float(input("Qual o faturamento do ano 2? "))
+faturamento3 = float(input("Qual o faturamento do ano 3? "))
+faturamento4 = float(input("Qual o faturamento do ano 4? "))
